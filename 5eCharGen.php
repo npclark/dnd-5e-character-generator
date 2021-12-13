8e07134d5205f927e0de6d60b66c2a6cd7606128
npclark <noah.clark500@gmail.com>
2021-12-12 19:33:15 -0600

    Character Generator
    Created the bulk of the website's processing.  This will draw information from the three tables in my database and display the randomly selected attributes

---
+++ 5eCharGen.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>5e Character Generator</title>
+
+    <style>
+        ul {
+            list-style-type: none;
+        }
+
+        li {
+            float: left;
+        }
+    </style>
+
+    <?php
+        // Class for the generated character
+        class Character {
+            public $raceName;
+            public $statBonus;
+            public $className;
+            public $classDesc;
+            public $hitDie;
+            public $primaryAbility;
+            public $savingThrows;
+            public $backgroundName;
+            public $backgroundFeature;
+            public $abilityScoreArray;
+
+            // Setters for each property
+            function setRaceName($raceName) {
+                $this->raceName = $raceName;
+            }
+            function setStatBonus($statBonus) {
+                $this->statBonus = $statBonus;
+            }
+            function setClassName($className) {
+                $this->className = $className;
+            }
+            function setClassDesc($classDesc) {
+                $this->classDesc = $classDesc;
+            }
+            function setHitDie($hitDie) {
+                $this->hitDie = $hitDie;
+            }
+            function setPrimaryAbility($primaryAbility) {
+                $this->primaryAbility = $primaryAbility;
+            }
+            function setSavingThrows($savingThrows) {
+                $this->savingThrows = $savingThrows;
+            }
+            function setBackgroundName($backgroundName) {
+                $this->backgroundName = $backgroundName;
+            }
+            function setBackgroundFeature($backgroundFeature) {
+                $this->backgroundFeature = $backgroundFeature;
+            }
+            function setAbilityScoreArray($abilityScoreArray) {
+                $this->abilityScoreArray = $abilityScoreArray;
+            }
+
+            // Getters for each property
+            function getRaceName() {
+                return $this->raceName;
+            }
+            function getStatBonus() {
+                return $this->statBonus;
+            }
+            function getClassName() {
+                return $this->className;
+            }
+            function getClassDesc() {
+                return $this->classDesc;
+            }
+            function getHitDie() {
+                return $this->hitDie;
+            }
+            function getPrimaryAbility() {
+                return $this->primaryAbility;
+            }
+            function getSavingThrows() {
+                return $this->savingThrows;
+            }
+            function getBackgroundName() {
+                return $this->backgroundName;
+            }
+            function getBackgroundFeature() {
+                return $this->backgroundFeature;
+            }
+            function getAbilityScoreArray() {
+                return $this->abilityScoreArray;
+            }
+        }
+    ?>
+</head>
+
+<body>
+    <?php include "database/dbConnect.php"; ?>
+    <header>
+        <h1>Dungeons and Dragons Fifth Edition Character Generator</h2>
+        <!--<ul>
+            <li>Home</li>
+            <li>Contact</li>
+            <li>Admin Login</li>
+        </ul>-->
+    </header>
+
+    <?php
+        if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+            // Gets maximum for use in randomization
+            $stmtRace = $conn->prepare("SELECT * FROM races");
+            $stmtRace->execute();
+            $stmtClass = $conn->prepare("SELECT * FROM classes");
+            $stmtClass->execute();
+            $stmtBack = $conn->prepare("SELECT * FROM backgrounds");
+            $stmtBack->execute();
+
+            // The following variables are used to select random records from the database, and is dynamic if more records are added to the tables
+            $raceMax = $stmtRace->rowCount();
+            $classMax = $stmtClass->rowCount();
+            $backMax = $stmtBack->rowCount();
+
+            // Selects a random record to be displayed later in the page
+            $stmtRace = $conn->prepare("SELECT * FROM races WHERE raceID=". rand(1,$raceMax));
+            $stmtRace->execute();
+            $stmtClass = $conn->prepare("SELECT * FROM classes WHERE classID=". rand(1,$classMax));
+            $stmtClass->execute();
+            $stmtBack = $conn->prepare("SELECT * FROM backgrounds WHERE backgroundID=". rand(1,$backMax));
+            $stmtBack->execute();
+
+            $resultRace = $stmtRace->fetch(PDO::FETCH_ASSOC);
+            $resultClass = $stmtClass->fetch(PDO::FETCH_ASSOC);
+            $resultBack = $stmtBack->fetch(PDO::FETCH_ASSOC);
+
+            // Generates ability score array
+            $abilityScores = " ";
+            for ($x = 0; $x < 6; $x++){
+                // Rolls four d6s
+                $roll = array(rand(1,6), rand(1,6), rand(1,6), rand(1,6));
+                
+                // Identifies lowest roll
+                $lowRoll = min($roll);
+
+                // This value will be set to true when the lowest roll is processed, in the event that a low number was rolled twice, ie the user rolled 3, 4, 3, and 5, only the first 3 will be ignored
+                $dupe = true;
+
+                for($d = 0; $d < 4; $d++){
+                    if($roll[$d] == $lowRoll && $dupe){
+                        $roll[$d] = 0;
+                        $dupe = false;
+                    }
+                }
+                $abilityScores .= array_sum($roll) . " | ";
+            }
+
+            // Creates the character to be sent to user
+            $character = new Character();    
+            $character->setRaceName($resultRace['raceName']);
+            $character->setStatBonus($resultRace['statBonus']);
+            $character->setClassName($resultClass['className']);
+            $character->setClassDesc($resultClass['classDesc']);
+            $character->setHitDie($resultClass['hitDie']);
+            $character->setPrimaryAbility($resultClass['primaryAbility']);
+            $character->setSavingThrows($resultClass['savingThrows']);
+            $character->setBackgroundName($resultBack['backgroundName']);
+            $character->setBackgroundFeature($resultBack['backgroundFeature']);
+            $character->setAbilityScoreArray($abilityScores);
+        }
+    ?>
+
+    <section>
+        <form id="charGen" name="charGen" method="post" action="5eCharGen.php">
+            <input type="submit" id="button" name="button" value="Generate Character">
+        </form>
+        <br>
+        <?php if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // Outputs the randomly selected records
+            echo "<p> Race: ", $character->getRaceName(), "<br>";
+            echo "Stat Bonus: ", $character->getStatBonus(), "<br>";
+            echo "Class: ", $character->getClassName(), "<br>";
+            echo "Description: ", $character->getClassDesc(), "<br>";
+            echo "Hit Dice: ", $character->getHitDie(), "<br>";
+            echo "Primary Ability: ", $character->getPrimaryAbility(), "<br>";
+            echo "Saving Throws: ", $character->getSavingThrows(), "<br>";
+            echo "Background: ", $character->getBackgroundName(), "<br>";
+            echo "Feature: ", $character->getBackgroundFeature(), "<br>";
+            echo "Ability Score Array:", $character->getAbilityScoreArray(), "</p>";
+        } ?>
+    </section>
+
+    <footer>
+        <h4><i>© <?php echo date("Y"); ?> </i></h4>
+    </footer>
+</body>
+</html>
